e8c60f9e7878e320048db5a795eeea795c5e4745
amirmuha <mirzaii4046@gmail.com>
2023-08-23 14:04:34 +0330

    update: exported types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,104 +1,109 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import React from 'react';
-import { BodyClasses } from 'components/table/body';
-import { HeaderClasses } from 'components/table/header';
-import { CellProps as CellProps$1 } from 'components/table/cell';
-import { TableColumn as TableColumn$1 } from 'components/table';
-import { TableRow as TableRow$1 } from 'components/table/row';
+import * as react_jsx_runtime from "react/jsx-runtime";
+import React from "react";
+import { BodyClasses } from "components/table/body";
+import { HeaderClasses } from "components/table/header";
+import { CellProps as CellProps$1 } from "components/table/cell";
+import { TableColumn as TableColumn$1 } from "components/table";
+import { TableRow as TableRow$1 } from "components/table/row";
 
-type RowClasses = {
+export type RowClasses = {
+  root?: string;
+  cell?: {
     root?: string;
-    cell?: {
-        root?: string;
-    };
+  };
 };
-type TableRow<T> = {
-    index: number;
+export type TableRow<T> = {
+  index: number;
 } & object;
-interface RowProps<T> {
-    row: TableRow<T>;
-    columns: TableColumn$1<T>[];
-    selection?: boolean;
-    onRowClick?: (info: TableRow<T>) => void;
-    cellProps?: Omit<CellProps$1<T>, "value">;
-    classes?: RowClasses;
-    overrideClasses?: RowClasses;
+export interface RowProps<T> {
+  row: TableRow<T>;
+  columns: TableColumn$1<T>[];
+  selection?: boolean;
+  onRowClick?: (info: TableRow<T>) => void;
+  cellProps?: Omit<CellProps$1<T>, "value">;
+  classes?: RowClasses;
+  overrideClasses?: RowClasses;
 }
 
-interface EditableCellInfo<T> {
-    row: TableRow$1<T>;
-    column: TableColumn$1<T>;
+export interface EditableCellInfo<T> {
+  row: TableRow$1<T>;
+  column: TableColumn$1<T>;
 }
 
-type CellClasses = {
-    root?: string;
+export type CellClasses = {
+  root?: string;
 };
-type CellInfo<T> = {
-    value: any;
-    row: TableRow$1<T>;
-    column: TableColumn$1<T>;
+export type CellInfo<T> = {
+  value: any;
+  row: TableRow$1<T>;
+  column: TableColumn$1<T>;
 };
-type CellProps<T> = {
-    value: any;
-    row: TableRow$1<T>;
-    column: TableColumn$1<T>;
-    selection?: boolean;
-    onCellClick?: (info: CellInfo<T>) => void;
-    onCellChange?: (info: EditableCellInfo<T>, value: any) => void;
-    classes?: CellClasses;
-    overrideClasses?: CellClasses;
-};
-
-type FooterClasses = {
-    root?: string;
-    cell?: CellClasses;
-    row?: RowClasses;
+export type CellProps<T> = {
+  value: any;
+  row: TableRow$1<T>;
+  column: TableColumn$1<T>;
+  selection?: boolean;
+  onCellClick?: (info: CellInfo<T>) => void;
+  onCellChange?: (info: EditableCellInfo<T>, value: any) => void;
+  classes?: CellClasses;
+  overrideClasses?: CellClasses;
 };
 
-type ContainerClasses = {
-    root?: string;
-    wrapper?: WrapperClasses;
+export type FooterClasses = {
+  root?: string;
+  cell?: CellClasses;
+  row?: RowClasses;
 };
-type WrapperClasses = {
-    root?: string;
-    table?: TableClasses;
+
+export type ContainerClasses = {
+  root?: string;
+  wrapper?: WrapperClasses;
 };
-type TableClasses = {
-    root?: string;
-    header?: HeaderClasses;
-    footer?: FooterClasses;
-    body?: BodyClasses;
+export type WrapperClasses = {
+  root?: string;
+  table?: TableClasses;
 };
-interface TableColumn<T> {
-    header: string;
-    name: keyof T;
-    width?: number;
-    minWidth?: number;
-    maxWidth?: number;
-    flex?: boolean;
-    editable?: boolean;
-    filterable?: boolean;
-    render?: (info: CellInfo<T>) => React.ReactNode;
+export type TableClasses = {
+  root?: string;
+  header?: HeaderClasses;
+  footer?: FooterClasses;
+  body?: BodyClasses;
+};
+export interface TableColumn<T> {
+  header: string;
+  name: keyof T;
+  width?: number;
+  minWidth?: number;
+  maxWidth?: number;
+  flex?: boolean;
+  editable?: boolean;
+  filterable?: boolean;
+  render?: (info: CellInfo<T>) => React.ReactNode;
 }
-type TableCellProps<T> = Omit<CellProps<T>, "value" | "row" | "column">;
-type TableRowProps<T> = Omit<RowProps<T>, "row" | "columns">;
-type TableProps<T> = {
-    data: T[];
-    columns: TableColumn<T>[];
-    selection?: boolean;
-    pagination?: boolean;
-    itemsPerPage?: number;
-    rowProps?: TableRowProps<T>;
-    cellProps?: TableCellProps<T>;
-    defaultSortedColumn?: keyof T | null;
-    defaultSortDirection?: "asc" | "desc";
-    classes?: {
-        container?: ContainerClasses;
-    };
-    overrideClasses?: {
-        container?: ContainerClasses;
-    };
+export type TableCellProps<T> = Omit<CellProps<T>, "value" | "row" | "column">;
+export type TableRowProps<T> = Omit<RowProps<T>, "row" | "columns">;
+export type TableProps<T> = {
+  data: T[];
+  columns: TableColumn<T>[];
+  selection?: boolean;
+  pagination?: boolean;
+  itemsPerPage?: number;
+  rowProps?: TableRowProps<T>;
+  cellProps?: TableCellProps<T>;
+  defaultSortedColumn?: keyof T | null;
+  defaultSortDirection?: "asc" | "desc";
+  classes?: {
+    container?: ContainerClasses;
+  };
+  overrideClasses?: {
+    container?: ContainerClasses;
+  };
 };
-declare const Table: <T extends object>({ pagination, defaultSortedColumn, defaultSortDirection, ...props }: TableProps<T>) => react_jsx_runtime.JSX.Element;
+declare const Table: <T extends object>({
+  pagination,
+  defaultSortedColumn,
+  defaultSortDirection,
+  ...props
+}: TableProps<T>) => react_jsx_runtime.JSX.Element;
 
 export { Table as AmirMuhaTable };
