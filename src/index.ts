af326c2d47e147c53760c49ecd6e7872cc6add02
amirmuha <mirzaii4046@gmail.com>
2023-08-24 10:42:01 +0330

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 import "./index.css";
 export { default as AmirMuhaTable } from "components/table";
-export * from "components/table/index";
-export * from "components/table/body/index";
-export * from "components/table/footer/index";
-export * from "components/table/header/index";
-export * from "components/table/cell/index";
-export * from "components/table/header/cell/editable-cell/index";
-export * from "components/table/row/index";
-export * from "components/table/pagination/index";
-export * from "components/table/index";
